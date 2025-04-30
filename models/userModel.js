60a07eb23185af334bade908710d5b43e5b2c6b1
mahesh <mahesh1995.sesetti@gmail.com>
2025-04-30 22:19:52 +0530

    updated data

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ export const users = () => {
     return [
         { id: 1, firstName: 'Alice', lastName: 'Smith', },
         { id: 2, firstName: 'Bob', lastName: 'Johnson' }
-    ]
+    ];
 };
 
 export const getUsers = () => users();
